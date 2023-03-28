6db243f3cec47f24e29086360759abbacf9adb9b
Alex <alexandrshestakov4@gmail.com>
2023-03-28 10:52:35 +0300

    commit2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 
 player1 = input("Select Rock, Paper, or Scissor :").lower()
 player2 = random.choice(["Rock", "Paper", "Scissor"]).lower()
+
 print("Player 2 selected: ", player2)
 
 if player1 == "rock" and player2 == "paper":
